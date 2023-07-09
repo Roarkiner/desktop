18b44882b3ff8a277ad7d4c23e89fd5399f4ca34
L.D <61159151+Roarkiner@users.noreply.github.com>
2023-07-09 17:56:42 +0200

    Logger

--- src/main/java/Connection.java
+++ src/main/java/Connection.java
@@ -7,19 +7,23 @@ import com.mongodb.client.MongoDatabase;
 import Enum.SexEnum;
 import Model.AthleteModel;
 import Repository.AthleteRepository;
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.Logger;
 
+import java.util.Date;
+
+import org.bson.BsonValue;
 import org.bson.codecs.configuration.CodecRegistry;
 import org.bson.codecs.pojo.PojoCodecProvider;
-import org.bson.types.ObjectId;
-import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import java.util.*;
 import static org.bson.codecs.configuration.CodecRegistries.fromProviders;
 import static org.bson.codecs.configuration.CodecRegistries.fromRegistries;
 
 public class Connection {
-    private static Logger logger = LoggerFactory.getLogger(Connection.class);
+    private static org.slf4j.Logger logger = LoggerFactory.getLogger(Connection.class);
     public static void main(String[] args) {
+        Logger mongoLogger = (Logger) LoggerFactory.getLogger("org.mongodb.driver");
+        mongoLogger.setLevel(Level.WARN);
         ConnectionString connectionString = new ConnectionString("mongodb+srv://application-desktop:6z7Z2i6K0sqYxG71@cluster0.aywre9k.mongodb.net/?retryWrites=true&w=majority");
         CodecRegistry pojoCodecRegistry = fromProviders(PojoCodecProvider.builder().automatic(true).build());
         CodecRegistry codecRegistry = fromRegistries(MongoClientSettings.getDefaultCodecRegistry(), pojoCodecRegistry);
@@ -38,14 +42,25 @@ public class Connection {
                 new Date(), 
                 SexEnum.UNDEFINED
             );
-            athleteRepository.saveAthlete(athleteToSave);
+            BsonValue savedAthleteId = athleteRepository.saveAthlete(athleteToSave);
 
-            AthleteModel athleteModelReturned = athleteRepository.getAthlete(athleteToSave.getId());
+            AthleteModel athleteModelReturned = athleteRepository.getAthlete(savedAthleteId.asObjectId().getValue());
             if(athleteModelReturned != null) {
-                logger.debug(athleteModelReturned.getFirstName() + " " + athleteModelReturned.getLastName());
+                String messageToLog = athleteModelReturned.getFirstName() + " " + athleteModelReturned.getLastName();
+                logger.info(messageToLog);
             } else {
                 logger.error("Athlete not found");
             }
+
+            athleteToSave.setLastName("HOIZEY");
+
+            
+        } catch(org.bson.BsonInvalidOperationException exception) {
+            logger.error("Impossible to convert _id to ObjectId", exception);
+        } catch(IllegalArgumentException exception) {
+            logger.error("Database not found", exception);
+        } catch(Exception exception) {
+            throw exception;
         }
     }
 }
